d60b41aed5022c197096e4fbd3505362560059da
darckwind <f.bunout01@ufromail.cl>
2018-05-04 16:29:02 -0300

    .

--- config/app.php
+++ config/app.php
@@ -209,6 +209,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::Class,
+        'Barryvdh' => Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
